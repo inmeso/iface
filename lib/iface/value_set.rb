d4b5e35dd27b012ca48bb3407ad18a53e22bdcf0
Jim Cain <jim@jcain.net>
2017-09-28 03:08:59 -0400

    Implemented []= and fetch_raw
    Added PrimaryInterface
    
    Factored out Pair into its own file

--- lib/iface/value_set.rb
+++ lib/iface/value_set.rb
@@ -1,30 +1,16 @@
 # frozen_string_literal: true
 
+require_relative 'value_set/pair'
+
 module Iface
-  # Represents a set of NAME=value pairs
+  # Represents a generic set of NAME=value pairs
   class ValueSet
-    # Represents a NAME=value pair
-    class Pair
-      attr_reader :name, :value
-
-      def initialize(line)
-        match = line.match(/(^[A-Z0-9_]+)="?(.*?)"?$/)
-        raise ArgumentError, "Expected pattern NAME=value; got #{line.inspect}" unless match
-        @name = match[1]
-        @value = match[2]&.sub(/^"/, '')&.sub(/"$/, '')
-      end
-
-      def to_s
-        "#{@name}=\"#{@value}\""
-      end
-    end
-
     def initialize(io)
       @vars = {}
       io.each_line do |line|
         edited_line = line.sub(/#.*$/, '').strip
         next if edited_line.empty?
-        pair = Pair.new(edited_line)
+        pair = Pair.create(edited_line, value_set: self)
         @vars[pair.name] = pair
       end
     end
@@ -33,13 +19,42 @@ module Iface
       @vars[name.upcase]&.value
     end
 
+    def []=(name, value)
+      key = name.upcase
+      if @vars.key?(key)
+        @vars[key].value = value
+      else
+        @vars[key] = Pair.create(key, value, value_set: self)
+      end
+    end
+
+    def fetch_raw(name)
+      @vars[name.upcase]&.raw_value
+    end
+
     def key?(name)
       @vars.key?(name.upcase)
     end
     alias has_key? key?
 
     def to_s
-      @vars.values.map(&:to_s).join("\n")
+      "#{@vars.values.map(&:to_s).join("\n")}\n"
+    end
+  end
+
+  # Represents a set of NAME=value pairs that would appear in a primary
+  # interface file, e.g. one named "ifcfg-eth0"
+  class PrimaryInterface < ValueSet
+    def make_static
+      self['BOOTPROTO'] = 'none'
+    end
+
+    def disable_nm
+      self['NM_CONTROLLED'] = 'no'
+    end
+
+    def use_ipv6
+      self['IPV6INIT'] = 'yes'
     end
   end
 end
--- spec/iface/value_set_spec.rb
+++ spec/iface/value_set_spec.rb
@@ -1,49 +1,20 @@
 # frozen_string_literal: true
 
-RSpec.describe Iface::ValueSet::Pair do
-  describe '::new' do
-    context 'bad syntax' do
-      it 'raises an ArgumentError' do
-        expect { described_class.new('foo') }.to raise_error(ArgumentError)
-        expect { described_class.new('FOO') }.to raise_error(ArgumentError)
-      end
-    end
-
-    context 'good syntax' do
-      it 'parses the text correctly' do
-        [
-          %w[FOO=bar FOO bar],
-          %w[FOO="baz" FOO baz],
-          ['FOO= baz', 'FOO', ' baz']
-        ].each do |text, name, value|
-          pair1 = described_class.new(text)
-          expect(pair1.name).to eq name
-          expect(pair1.value).to eq value
-        end
-      end
-    end
-  end
-
-  describe '#to_s' do
-    it 'returns NAME=value' do
-      [
-        %w[FOO=bar FOO="bar"],
-        %w[FOO="baz" FOO="baz"],
-        ['FOO= baz', 'FOO=" baz"']
-      ].each do |text, result|
-        expect(described_class.new(text).to_s).to eq result
-      end
-    end
-  end
-end
-
 RSpec.describe Iface::ValueSet do
+  let(:ipaddr) { '192.168.10.1' }
+  let(:ipaddr_raw) { 192 * 256**3 + 168 * 256**2 + 10 * 256 + 1 }
+
+  let(:hash) do
+    {
+      'FOO' => 'bar',
+      'BAR' => 'baz',
+      'PHRED' => 'smerd',
+      'IPADDR' => ipaddr
+    }
+  end
+
   let(:value_set) do
-    described_class.new(StringIO.new(<<~__EOF__))
-      FOO=bar
-      BAR="baz"
-      PHRED=smerd
-    __EOF__
+    described_class.new(StringIO.new(hash.map { |k, v| %(#{k}="#{v}") }.join("\n")))
   end
 
   describe '#[]' do
@@ -59,6 +30,42 @@ RSpec.describe Iface::ValueSet do
       it 'returns nil' do
         expect(value_set['baz']).to be_nil
         expect(value_set['smerd']).to be_nil
+      end
+    end
+  end
+
+  describe '#[]=' do
+    context 'name exists' do
+      it 'updates the value' do
+        expect(value_set['PHRED']).to eq 'smerd'
+        value_set['PHRED'] = 'smith'
+        expect(value_set['PHRED']).to eq 'smith'
+      end
+    end
+
+    context 'name does not exist' do
+      it 'creates the name/value pair' do
+        expect(value_set).to_not have_key 'ALPHA'
+        value_set['ALPHA'] = 'bravo'
+        expect(value_set['ALPHA']).to eq 'bravo'
+      end
+    end
+  end
+
+  describe '#fetch_raw' do
+    context 'name exists' do
+      it 'returns the raw value' do
+        expect(value_set.fetch_raw('foo')).to eq 'bar'
+        expect(value_set.fetch_raw('bar')).to eq 'baz'
+        expect(value_set.fetch_raw('PHRED')).to eq 'smerd'
+        expect(value_set.fetch_raw('ipaddr')).to eq ipaddr_raw
+      end
+    end
+
+    context 'name does not exist' do
+      it 'returns nil' do
+        expect(value_set.fetch_raw('baz')).to be_nil
+        expect(value_set.fetch_raw('smerd')).to be_nil
       end
     end
   end
@@ -82,7 +89,82 @@ RSpec.describe Iface::ValueSet do
 
   describe '#to_s' do
     it 'returns a deserialized list of NAME=value pairs' do
-      expect(value_set.to_s).to eq %(FOO="bar"\nBAR="baz"\nPHRED="smerd")
+      expect(value_set.to_s).to eq %(FOO="bar"\nBAR="baz"\nPHRED="smerd"\nIPADDR="192.168.10.1"\n)
+    end
+  end
+end
+
+RSpec.describe Iface::PrimaryInterface do
+  let(:ipaddr) { '192.168.10.1' }
+  # let(:ipaddr_raw) { 192 * 256**3 + 168 * 256**2 + 10 * 256 + 1 }
+
+  let(:hash) do
+    {
+      'BOOTPROTO' => 'dhcp',
+      'NM_CONTROLLED' => 'yes',
+      'IPADDR' => ipaddr,
+      'IPV6INIT' => 'no'
+    }
+  end
+
+  let(:value_set) do
+    described_class.new(StringIO.new(hash.map { |k, v| %(#{k}="#{v}") }.join("\n")))
+  end
+
+  describe '#make_static' do
+    context 'BOOTPROTO does not exist' do
+      it 'adds BOOTPROTO=none to the set' do
+        hash.delete('BOOTPROTO')
+        expect(value_set).to_not have_key 'BOOTPROTO'
+        value_set.make_static
+        expect(value_set['bootproto']).to eq 'none'
+      end
+    end
+
+    context 'BOOTPROTO exists' do
+      it 'updates the value of BOOTPROTO to none' do
+        expect(value_set['bootproto']).to eq 'dhcp'
+        value_set.make_static
+        expect(value_set['bootproto']).to eq 'none'
+      end
+    end
+  end
+
+  describe '#disable_nm' do
+    context 'NM_CONTROLLED does not exist' do
+      it 'adds NM_CONTROLLED=no to the set' do
+        hash.delete('NM_CONTROLLED')
+        expect(value_set).to_not have_key 'NM_CONTROLLED'
+        value_set.disable_nm
+        expect(value_set['nm_controlled']).to eq 'no'
+      end
+    end
+
+    context 'NM_CONTROLLED exists' do
+      it 'updates the value of NM_CONTROLLED to no' do
+        expect(value_set['nm_controlled']).to eq 'yes'
+        value_set.disable_nm
+        expect(value_set['nm_controlled']).to eq 'no'
+      end
+    end
+  end
+
+  describe '#use_ipv6' do
+    context 'IPV6INIT does not exist' do
+      it 'adds IPV6INIT=yes to the set' do
+        hash.delete('IPV6INIT')
+        expect(value_set).to_not have_key 'IPV6INIT'
+        value_set.use_ipv6
+        expect(value_set['ipv6init']).to eq 'yes'
+      end
+    end
+
+    context 'IPV6INIT exists' do
+      it 'updates the value of IPV6INIT to yes' do
+        expect(value_set['ipv6init']).to eq 'no'
+        value_set.use_ipv6
+        expect(value_set['ipv6init']).to eq 'yes'
+      end
     end
   end
 end
