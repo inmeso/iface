38148625e572685795727c3af92291290ee3a804
Jim Cain <jim@jcain.net>
2017-09-28 00:34:08 -0400

    Filled out IpAddress implementation and spec

--- lib/iface/ip_address.rb
+++ lib/iface/ip_address.rb
@@ -3,12 +3,97 @@
 require 'ipaddr'
 
 module Iface
-  # Represents an IP address including subnet mask
+  # Represents an IP address including prefix length
   class IpAddress
-    def initialize(*args)
-      @ipaddr = IPAddr.new(*args)
-      @_bitmask = @ipaddr.instance_eval { @mask_addr.to_s(2) }
+    def self.create(str)
+      ipaddr = IPAddr.new(str)
+      (ipaddr.ipv4? ? IpV4Address : IpV6Address).new(ipaddr)
+    end
+
+    def initialize(ip_address)
+      @ipaddr = ip_address.is_a?(IPAddr) ? ip_address : IPAddr.new(ip_address)
+      @mask_addr = @ipaddr.instance_eval { @mask_addr }
+      @_bitmask = @mask_addr.to_s(2)
       @inverse_mask_addr = @_bitmask.tr('01', '10').to_i(2)
+    end
+
+    def cidr_mask
+      case diff = full_mask - @mask_addr
+      when 0
+        full_cidr_mask
+      when 1
+        full_cidr_mask - 1
+      else
+        full_cidr_mask - Math.log2(diff).ceil
+      end
+    end
+
+    def to_i
+      @ipaddr.to_i
+    end
+
+    def to_s
+      @ipaddr.to_s
+    end
+
+    def full_mask
+      raise NotImplementedError
+    end
+
+    def full_cidr_mask
+      raise NotImplementedError
+    end
+
+    def ==(other)
+      self.class == other.class && __state__ == other.__state__
+    end
+
+    protected
+
+    def __state__
+      @ipaddr
+    end
+  end
+
+  # Represents an IPv4 address
+  class IpV4Address < IpAddress
+    def self.from_numeric(numeric)
+      new(IPAddr.new_ntoh([numeric].pack('N')))
+    end
+
+    def initialize(ip_address)
+      super
+      raise ArgumentError, "Expected IPv4, got IPv6: #{ip_address}" unless @ipaddr.ipv4?
+    end
+
+    def full_mask
+      IPAddr::IN4MASK
+    end
+
+    def full_cidr_mask
+      32
+    end
+  end
+
+  # Represents an IPv6 address
+  class IpV6Address < IpAddress
+    MASK_32 = 0xffffffffffffffff
+
+    def self.from_numeric(numeric)
+      new(IPAddr.new_ntoh([(numeric >> 96), (numeric >> 64) & MASK_32, (numeric >> 32) & MASK_32, numeric & MASK_32].pack('N*')))
+    end
+
+    def initialize(ip_address)
+      super
+      raise ArgumentError, "Expected IPv6, got IPv4: #{ip_address}" unless @ipaddr.ipv6?
+    end
+
+    def full_mask
+      IPAddr::IN6MASK
+    end
+
+    def full_cidr_mask
+      128
     end
   end
 end
--- spec/iface/ip_address_spec.rb
+++ spec/iface/ip_address_spec.rb
@@ -1,13 +1,122 @@
 # frozen_string_literal: true
 
+RSpec.shared_context 'IPv4' do
+  let(:ip4_string) { '192.168.10.10' }
+  let(:ip4_int) { 192 * 256**3 + 168 * 256**2 + 10 * 256 + 10 }
+end
+
+RSpec.shared_context 'IPv6' do
+  let(:ip6_string) { '2001:abcd:1234::2' }
+  let(:ip6_int) do
+    '20'.to_i(16) * 256**15 +
+      '01'.to_i(16) * 256**14 +
+      'ab'.to_i(16) * 256**13 +
+      'cd'.to_i(16) * 256**12 +
+      '12'.to_i(16) * 256**11 +
+      '34'.to_i(16) * 256**10 +
+      2
+  end
+end
+
 RSpec.describe Iface::IpAddress do
-  let(:ip_address) { described_class.new(ip_string) }
+  include_context 'IPv4'
+  include_context 'IPv6'
 
-  context '?' do
-    let(:ip_string) { '192.168.10.1' }
+  let(:ip4_mask) { 24 }
+  let(:ip4_string_with_mask) { "#{ip4_string}/#{ip4_mask}" }
+  let(:ip4_string_with_full_mask) { "#{ip4_string}/32" }
 
-    it 'does something' do
-      expect(ip_address).to be_truthy
+  let(:ip6_mask) { 48 }
+  let(:ip6_string_with_mask) { "#{ip6_string}/#{ip6_mask}" }
+  let(:ip6_string_with_full_mask) { "#{ip6_string}/128" }
+
+  # let(:ip_address) { described_class.new(ip_string) }
+
+  describe '::create' do
+    context 'with IPv4 string' do
+      it 'returns an instance of IpV4Address' do
+        expect(described_class.create(ip4_string)).to be_an Iface::IpV4Address
+      end
+    end
+
+    context 'with IPv6 string' do
+      it 'returns an instance of IpV6Address' do
+        expect(described_class.create(ip6_string)).to be_an Iface::IpV6Address
+      end
+    end
+  end
+
+  describe '#cidr_mask' do
+    context 'with IPv4 string with mask' do
+      it 'returns the mask' do
+        expect(described_class.create(ip4_string_with_mask).cidr_mask).to eq ip4_mask
+      end
+    end
+
+    context 'with IPv4 string with full mask' do
+      it 'returns the mask' do
+        expect(described_class.create(ip4_string_with_full_mask).cidr_mask).to eq 32
+      end
+    end
+
+    context 'with IPv6 string with mask' do
+      it 'returns the mask' do
+        expect(described_class.create(ip6_string_with_mask).cidr_mask).to eq ip6_mask
+      end
+    end
+
+    context 'with IPv6 string with full mask' do
+      it 'returns the mask' do
+        expect(described_class.create(ip6_string_with_full_mask).cidr_mask).to eq 128
+      end
+    end
+  end
+
+  describe '#full_mask' do
+    context 'with IPv4 string' do
+      it 'returns 2^32 - 1' do
+        expect(described_class.create(ip4_string).full_mask).to eq 2**32 - 1
+      end
+    end
+
+    context 'with IPv6 string' do
+      it 'returns 2^128 - 1' do
+        expect(described_class.create(ip6_string).full_mask).to eq 2**128 - 1
+      end
+    end
+  end
+
+  describe '#to_i' do
+    context 'with IPv4 string' do
+      it 'returns the integer value' do
+        expect(described_class.create(ip4_string).to_i).to eq ip4_int
+      end
+    end
+
+    context 'with IPv6 string' do
+      it 'returns the integer value' do
+        expect(described_class.create(ip6_string).to_i).to eq ip6_int
+      end
+    end
+  end
+end
+
+RSpec.describe Iface::IpV4Address do
+  include_context 'IPv4'
+
+  describe '::from_numeric' do
+    it 'returns the IpV4Address' do
+      expect(described_class.from_numeric(ip4_int)).to eq described_class.new(ip4_string)
+    end
+  end
+end
+
+RSpec.describe Iface::IpV6Address do
+  include_context 'IPv6'
+
+  describe '::from_numeric' do
+    it 'returns the IpV6Address' do
+      expect(described_class.from_numeric(ip6_int)).to eq described_class.new(ip6_string)
     end
   end
 end
